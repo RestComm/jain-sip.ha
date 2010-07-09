bb2ebf71d38ef772045e0878a9df98669567dd33
jean.deruelle <jean.deruelle@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-07-09 13:07:43 +0000

    Update Issue 1601
    Don't thrown exception for Tomcat, ust log it as a warning
    
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/servers/jain-sip-ha@12838 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- jboss-4/src/main/java/org/mobicents/ha/javax/sip/SipStackImpl.java
+++ jboss-4/src/main/java/org/mobicents/ha/javax/sip/SipStackImpl.java
@@ -25,6 +25,7 @@ import gov.nist.core.StackLogger;
 
 import java.util.Properties;
 
+import javax.management.InstanceNotFoundException;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerFactory;
 import javax.management.Notification;
@@ -88,15 +89,19 @@ public class SipStackImpl extends ClusteredSipStackImpl implements NotificationL
 		try {
 			oname = new ObjectName(mBeanName);
 			if (getMBeanServer() != null && !getMBeanServer().isRegistered(oname)) {
-				getMBeanServer().registerMBean(this, oname);
-				if(getStackLogger().isLoggingEnabled(StackLogger.TRACE_INFO)) {
-					getStackLogger().logInfo("Adding notification listener for logging mbean \"" + LOG4J_SERVICE_MBEAN_NAME + "\" to server " + getMBeanServer());
-				}
-				getMBeanServer().addNotificationListener(new ObjectName(LOG4J_SERVICE_MBEAN_NAME), this, null, null);
+				getMBeanServer().registerMBean(this, oname);							
 			}
 		} catch (Exception e) {
 			getStackLogger().logError("Could not register the stack as an MBean under the following name", e);
 			throw new SipException("Could not register the stack as an MBean under the following name " + mBeanName + ", cause: " + e.getMessage(), e);
+		}
+		try {
+			if(getStackLogger().isLoggingEnabled(StackLogger.TRACE_INFO)) {
+				getStackLogger().logInfo("Adding notification listener for logging mbean \"" + LOG4J_SERVICE_MBEAN_NAME + "\" to server " + getMBeanServer());
+			}
+			getMBeanServer().addNotificationListener(new ObjectName(LOG4J_SERVICE_MBEAN_NAME), this, null, null);
+		} catch (Exception e) {
+			getStackLogger().logWarning("Could not register the stack as a Notification Listener of " + LOG4J_SERVICE_MBEAN_NAME + " runtime changes to log4j.xml won't affect SIP Stack Logging");
 		}
 	}
 	
